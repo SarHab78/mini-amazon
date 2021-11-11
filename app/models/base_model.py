5e5d16391e52ca2a82e07a76ca8a317bfe98d901
Sarah Habib <sjh78@duke.edu>
2021-11-11 12:50:07 -0500

    updated models page and added a seller button

---
+++ app/models/base_model.py
@@ -0,0 +1,132 @@
+from flask_login import UserMixin
+from flask import current_app as app
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from .. import login
+
+#user table information
+class User(UserMixin):
+    def __init__(self, id, email, firstname, lastname):
+        self.id = id
+        self.email = email
+        self.firstname = firstname
+        self.lastname = lastname
+
+    @staticmethod
+    def get_by_auth(email, password):
+        rows = app.db.execute("""
+SELECT password, id, email, firstname, lastname
+FROM Users
+WHERE email = :email
+""",
+                              email=email)
+        if not rows:  # email not found
+            return None
+        elif not check_password_hash(rows[0][0], password):
+            # incorrect password
+            return None
+        else:
+            return User(*(rows[0][1:]))
+
+    @staticmethod
+    def email_exists(email):
+        rows = app.db.execute("""
+SELECT email
+FROM Users
+WHERE email = :email
+""",
+                              email=email)
+        return len(rows) > 0
+
+    @staticmethod
+    def register(email, password, firstname, lastname):
+        try:
+            rows = app.db.execute("""
+INSERT INTO Users(email, password, firstname, lastname)
+VALUES(:email, :password, :firstname, :lastname)
+RETURNING id
+""",
+                                  email=email,
+                                  password=generate_password_hash(password),
+                                  firstname=firstname,
+                                  lastname=lastname)
+            id = rows[0][0]
+            return User.get(id)
+        except Exception:
+            # likely email already in use; better error checking and
+            # reporting needed
+            return None
+
+    @staticmethod
+    @login.user_loader
+    def get(id):
+        rows = app.db.execute("""
+SELECT id, email, firstname, lastname
+FROM Users
+WHERE id = :id
+""",
+                              id=id)
+        return User(*(rows[0])) if rows else None
+
+
+
+#Purchase table information
+        
+class Purchase:
+    def __init__(self, id, uid, pid, time_purchased):
+        self.id = id
+        self.uid = uid
+        self.pid = pid
+        self.time_purchased = time_purchased
+
+    @staticmethod
+    def get(id):
+        rows = app.db.execute('''
+SELECT id, uid, pid, time_purchased
+FROM Purchases
+WHERE id = :id
+''',
+                              id=id)
+        return Purchase(*(rows[0])) if rows else None
+
+    @staticmethod
+    def get_all_by_uid_since(uid, since):
+        rows = app.db.execute('''
+SELECT id, uid, pid, time_purchased
+FROM Purchases
+WHERE uid = :uid
+AND time_purchased >= :since
+ORDER BY time_purchased DESC
+''',
+                              uid=uid,
+                              since=since)
+        return [Purchase(*row) for row in rows]
+
+
+#Product table information
+class Product:
+    def __init__(self, id, name, price, available):
+        self.id = id
+        self.name = name
+        self.price = price
+        self.available = available
+
+    @staticmethod
+    def get(id):
+        rows = app.db.execute('''
+SELECT id, name, price, available
+FROM Products
+WHERE id = :id
+''',
+                              id=id)
+        return Product(*(rows[0])) if rows is not None else None
+
+    @staticmethod
+    def get_all(available=True):
+        rows = app.db.execute('''
+SELECT id, name, price, available
+FROM Products
+WHERE available = :available
+''',
+                              available=available)
+        return [Product(*row) for row in rows]
