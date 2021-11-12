df13db037d3451b9c4f671fa4be765a5f53ff913
rnt6 <rnt6@duke.edu>
2021-11-11 20:49:22 -0500

    resetting to users to try

--- app/reviews.py
+++ app/reviews.py
@@ -2,38 +2,84 @@
 #write a review
 #edit
 #add in access restriction to only those who have purchased
-
 from flask import render_template, redirect, url_for, flash, request
 from werkzeug.urls import url_parse
 from flask_login import login_user, logout_user, current_user
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField
 from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
 from flask_babel import _, lazy_gettext as _l
 
-from .models.review import Product_review
+from .models.base_model import User
 
 
 from flask import Blueprint
-bp = Blueprint('reviews', __name__)
+bp = Blueprint('users', __name__)
 
-class AddAReview(FlaskForm):
-    email = StringField(_l('email'), validators=[DataRequired(), Email()])
-    rating = IntegerField(_l('rating'), validators=[DataRequired()])
-    pid = IntegerField(_l('rating'), validators=[DataRequired()])
-    review = StringField(_l('rating'), validators=[DataRequired()])
-    submit = SubmitField(_l('Submit'))
-    # def validate_email(self, email): when we have cart functionality - Jo?
 
-@bp.route('/post_review', methods=['GET', 'POST'])
-def post_review():
-    form = AddAReview()
+class LoginForm(FlaskForm):
+    email = StringField(_l('Email'), validators=[DataRequired(), Email()])
+    password = PasswordField(_l('Password'), validators=[DataRequired()])
+    remember_me = BooleanField(_l('Remember Me'))
+    submit = SubmitField(_l('Sign In'))
+
+
+@bp.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index.index'))
+    form = LoginForm()
     if form.validate_on_submit():
-        if Product_review.get_prod(
-                         form.pid.data,
-                         form.email.data,
-                         form.rating.data,
-                         form.review.data):
-            flash('Review Posted')
-            return redirect(url_for('index.index'))
-    return render_template('review_form.html', title='Post Review', form=form)
+        user = User.get_by_auth(form.email.data, form.password.data)
+        if user is None:
+            flash('Invalid email or password')
+            return redirect(url_for('users.login'))
+        login_user(user)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index.index')
+
+        return redirect(next_page)
+    return render_template('login.html', title='Sign In', form=form)
+
+
+class RegistrationForm(FlaskForm):
+    firstname = StringField(_l('First Name'), validators=[DataRequired()])
+    lastname = StringField(_l('Last Name'), validators=[DataRequired()])
+    email = StringField(_l('Email'), validators=[DataRequired(), Email()])
+    address = StringField(_l('Address'), validators=[DataRequired()])
+    balance = IntegerField(_l('Balance'), validators=[DataRequired()])
+    is_seller = StringField(_l('Seller?'), validators=[DataRequired()])
+    password = PasswordField(_l('Password'), validators=[DataRequired()])
+    password2 = PasswordField(
+        _l('Repeat Password'), validators=[DataRequired(),
+                                           EqualTo('password')])
+    submit = SubmitField(_l('Register'))
+
+    def validate_email(self, email):
+        if User.email_exists(email.data):
+            raise ValidationError(_('Already a user with this email.'))
+
+
+@bp.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index.index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        if User.register(form.email.data,
+                         form.password.data,
+                         form.firstname.data,
+                         form.lastname.data,
+                         form.address.data,
+                         form.balance.data,
+                         form.is_seller.data):
+            flash('Congratulations, you are now a registered user!')
+            return redirect(url_for('users.login'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@bp.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index.index'))
