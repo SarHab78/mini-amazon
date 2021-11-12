ef2a70ad25fbbeebdc39d193a17b6f6982cc6c7c
fpn <fpn@duke.edu>
2021-11-11 20:55:30 -0500

    Update reviews.py

--- app/reviews.py
+++ app/reviews.py
@@ -2,40 +2,39 @@
 #write a review
 #edit
 #add in access restriction to only those who have purchased
+
 from flask import render_template, redirect, url_for, flash, request
 from werkzeug.urls import url_parse
 from flask_login import login_user, logout_user, current_user
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
 from flask_babel import _, lazy_gettext as _l
 
-from .models.base_model import User
+from .models.review import Product_review
 
 
 from flask import Blueprint
-bp = Blueprint('users', __name__)
+bp = Blueprint('reviews', __name__)
 
-
-class ReviewForm(FlaskForm):
-    email = StringField(_l('Email'), validators=[DataRequired(), Email()])
-    pid = IntegerField(_l('Product ID'), validators=[DataRequired()])
-    rev = StringField(_l('Review'), validators=[DataRequired()])
-    rating = IntegerField(_l('Rating'), validators=[DataRequired()])
+class AddAReview(FlaskForm):
+    email = StringField(_l('email'), validators=[DataRequired(), Email()])
+    rating = IntegerField(_l('rating'), validators=[DataRequired()])
+    pid = IntegerField(_l('rating'), validators=[DataRequired()])
+    review = StringField(_l('rating'), validators=[DataRequired()])
     submit = SubmitField(_l('Submit'))
+    # def validate_email(self, email): when we have cart functionality - Jo?
 
-
-@bp.route('/register', methods=['GET', 'POST'])
-def register():
-    if current_user.is_authenticated:
-        return redirect(url_for('index.index'))
-    form = RegistrationForm()
+@bp.route('/post_review', methods=['GET', 'POST'])
+def post_review():
+    form = AddAReview()
     if form.validate_on_submit():
-        if User.register(form.email.data,
+        if Product_review.get_prod(
                          form.pid.data,
-                         form.rev.data,
-                         form.rating.data
-        ):
-            flash('Congratulations, you submitted a review!')
-            return redirect(url_for('users.login'))
-    return render_template('review_form.html', title='review_form', form=form)
+                         form.email.data,
+                         form.rating.data,
+                         form.review.data):
+            flash('Review Posted')
+            return redirect(url_for('index.index'))
+    return render_template('review_form.html', title='Post Review', form=form)
+
