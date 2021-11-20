5c7ef85445d898573571c6c4cb6056e3aea88720
rnt6 <rnt6@duke.edu>
2021-11-20 14:58:12 -0500

    Update load.sql

--- db/load.sql
+++ db/load.sql
@@ -1,3 +1,3 @@
-\COPY Users FROM 'data/Users.csv' WITH DELIMITER ',' NULL '' CSV
+\COPY Users FROM 'data/Users_stress_test_database.csv' WITH DELIMITER ',' NULL '' CSV
 \COPY Products FROM 'data/products_stress_test.csv' WITH DELIMITER ',' NULL '' CSV
 --\COPY Purchases FROM 'data/Purchases.csv' WITH DELIMITER ',' NULL '' CSV
