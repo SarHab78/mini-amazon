238d6a0a3547b81a62d3815810578cbd0238b6d0
rnt6 <rnt6@duke.edu>
2021-11-20 16:01:49 -0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -27,4 +27,7 @@ def create_app():
     from .seller_inventory import bp as si_bp
     app.register_blueprint(si_bp)
 
+    from .profile import bp as profile_bp
+    app.register_blueprint(profile_bp)
+
     return app
