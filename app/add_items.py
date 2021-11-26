d0122fcbc6e5b5cf9f0d4f3ce1db773c859afe60
Sarah Habib <sjh78@duke.edu>
2021-11-26 00:50:25 -0500

    added an add inventory page and routing is working, seems to be an error with the products stress-test database that we need to fix

--- app/add_items.py
+++ app/add_items.py
@@ -2,7 +2,7 @@ from flask import render_template
 from flask_login import current_user
 import datetime
 
-from .models.base_model import Product
+#from .models.base_model import Product
 
 from flask import Blueprint
 bp = Blueprint('add_items', __name__)
