f7d5a87b592c7fd1e3e66393468491ce3eaf4fab
Flannery Nania <fpn@duke.edu>
2021-11-28 17:25:08 -0500

    change import statement

--- app/reviews.py
+++ app/reviews.py
@@ -11,7 +11,7 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
 from flask_babel import _, lazy_gettext as _l
 
-from .models.review import Product_review
+from .models.base_model import Product_review
 
 
 from flask import Blueprint
