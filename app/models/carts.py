a46d02e4e961548e7c070945f2f7193d9ab718b1
js890 <js890@duke.edu>
2021-11-28 17:20:06 -0500

    route back home

--- app/models/carts.py
+++ app/models/carts.py
@@ -1,1 +1,13 @@
+from flask import render_template
+from flask_login import current_user
+import datetime
 
+from .models.product import Product
+from .models.purchase import Purchase
+
+from flask import Blueprint
+bp = Blueprint('carts', __name__)
+
+@bp.route('/')
+def main_page():
+    return render_template('index.html')
