a0918d45b96370627744f703c37eff7533471c29
Flannery Nania <fpn@duke.edu>
2021-11-28 15:28:18 -0500

    init fix

---
+++ app/models/__init__.py
@@ -0,0 +1,33 @@
+from flask import Flask
+from flask_login import LoginManager
+from flask_babel import Babel
+from .config import Config
+from .db import DB
+
+
+login = LoginManager()
+login.login_view = 'users.login'
+babel = Babel()
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    app.db = DB(app)
+    login.init_app(app)
+    babel.init_app(app)
+
+    from .index import bp as index_bp
+    app.register_blueprint(index_bp)
+
+    from .users import bp as user_bp
+    app.register_blueprint(user_bp)
+
+    from .seller_inventory import bp as si_bp
+    app.register_blueprint(si_bp)
+
+    from .add_items import bp as add_bp
+    app.register_blueprint(add_bp)
+
+    return app
