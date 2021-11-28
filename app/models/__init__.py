20cc3978c77c5484a00f87c5b1f6bb3315f6e93b
Flannery Nania <fpn@duke.edu>
2021-11-28 15:39:00 -0500

    there are two inits. idk what to do with this information

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -14,20 +14,23 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
+    # ADD A SECRET KEY LINE - SOPHIE - this is to try to make "session" work
+    app.secret_key = "any random string"
+
     app.db = DB(app)
     login.init_app(app)
     babel.init_app(app)
 
     from .index import bp as index_bp
     app.register_blueprint(index_bp)
-
+    
     from .users import bp as user_bp
     app.register_blueprint(user_bp)
 
-    from .seller_inventory import bp as si_bp
-    app.register_blueprint(si_bp)
+    from .product_page import bp as product_page_bp
+    app.register_blueprint(product_page_bp)
 
-    from .add_items import bp as add_bp
-    app.register_blueprint(add_bp)
+    from .reviews import bp as reviews_bp
+    app.register_blueprint(reviews_bp)
 
     return app
