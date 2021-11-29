c23872ac29bb14ce5f3c5e9be45ecfe08f0e2fd8
Flannery Nania <fpn@duke.edu>
2021-11-28 19:03:54 -0500

    commented out some of create for the sake of testing the db

--- db/load.sql
+++ db/load.sql
@@ -1,5 +1,5 @@
 \COPY Users FROM 'data/Users_stress_test_database.csv' WITH DELIMITER ',' NULL '' CSV
 \COPY Products FROM 'data/products_stress_test.csv' WITH DELIMITER ',' NULL '' CSV
 --\COPY Purchases FROM 'data/Purchases.csv' WITH DELIMITER ',' NULL '' CSV
-\COPY product_review FROM 'data/review_sample_DB' WITH DELIMITER ',' NULL '' CSV
+\COPY product_review FROM 'data/review_sample_DB_-_Sheet1.csv' WITH DELIMITER ',' NULL '' CSV
 
