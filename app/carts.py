013dd545a7b13275dc47c17394f17e59e11ae2ed
js890 <js890@duke.edu>
2021-11-28 19:40:34 -0500

    added chckout

--- app/carts.py
+++ app/carts.py
@@ -10,7 +10,10 @@ from flask import Blueprint
 bp = Blueprint('carts', __name__)
 
 
-
+@bp.route('/checkout')
+def checkout():
+    products = Product.get_all('Y')
+    return render_template('checkout.html')
 
 
 
