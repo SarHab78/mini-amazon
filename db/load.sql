472a88db83fdccd90b2a98358a2154d0c45c8436
js890 <js890@duke.edu>
2021-11-30 12:20:17 -0500

    Update load.sql

--- db/load.sql
+++ db/load.sql
@@ -1,3 +1,6 @@
 \COPY Users FROM 'data/Users_stress_test_dataset.csv' WITH DELIMITER ',' NULL '' CSV
 \COPY Products FROM 'data/products_stress_test.csv' WITH DELIMITER ',' NULL '' CSV
 --\COPY Purchases FROM 'data/Purchases.csv' WITH DELIMITER ',' NULL '' CSV
+\COPY product_review FROM 'data/review_sample_DB_-_Sheet1.csv' WITH DELIMITER ',' NULL '' CSV
+
+
