4395cb22f96bc85412f1acbbe7c4b1d8b91de87f
rnt6 <rnt6@duke.edu>
2021-12-01 14:39:57 -0500

    still trying to fix edit submission ahhhhh

--- app/models/base_model.py
+++ app/models/base_model.py
@@ -98,7 +98,7 @@ WHERE id = :id
         return User(*(rows[0])) if rows else None
 
     @staticmethod
-    def editprofile(email, password, firstname, lastname, address, balance, is_seller):
+    def editprofile(id, email, password, firstname, lastname, address, balance, is_seller):
         try:
             rows = app.db.execute("""
 "UPDATE Users
@@ -111,7 +111,8 @@ WHERE id = :id
                                   lastname=lastname,
                                   address= address,
                                   balance = balance,
-                                  is_seller = is_seller)
+                                  is_seller = is_seller,
+                                  id= id)
             id = rows[0][0]
             return User.get(id)
         except Exception:
