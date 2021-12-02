d2cba314af8755f61e360d6919e28b66377892ab
js890 <js890@duke.edu>
2021-12-02 13:07:45 -0500

    Update carts.py

--- app/carts.py
+++ app/carts.py
@@ -13,13 +13,13 @@ bp = Blueprint('carts', __name__)
 
 
 
-@bp.route('/')
-def carts():
-    if current_user.is_authenticated:
-        my_cart = Orders.get_cart(uid)
-    else:
-        my_cart = NULL
-    return render_template('carts.html', user_cart = my_cart)
+# @bp.route('/')
+# def carts():
+#     if current_user.is_authenticated:
+#         my_cart = Orders.get_cart(uid)
+#     else:
+#         my_cart = NULL
+#     return render_template('carts.html', user_cart = my_cart)
 
 
 @bp.route('/checkout')
