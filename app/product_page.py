8edbb2fd0a7c74c03e3033aaac502c17c9b1b83f
js890 <js890@duke.edu>
2021-12-02 15:00:21 -0500

    Update product_page.py

--- app/product_page.py
+++ app/product_page.py
@@ -29,10 +29,10 @@ def product_page(name, product_id):
                             num_reviews=num_reviews)
 
 
-@bp.route('/<name>/<product_id>_added_to_cart')
-def added_to_cart():
-    my_cart = add_to_cart(product_id, quantity, uid)
-    return render_template('interim_added_cart_page.html')
+# @bp.route('/<name>/<product_id>_added_to_cart')
+# def added_to_cart():
+#     my_cart = add_to_cart(product_id, quantity, uid)
+#     return render_template('interim_added_cart_page.html')
 # , user_cart = my_cart, new = new_item
 
 
