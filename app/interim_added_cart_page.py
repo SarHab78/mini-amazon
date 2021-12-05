b7d9d1778b98562448f4eec866658491e90c27bd
js890 <js890@duke.edu>
2021-12-05 18:38:47 -0500

    Update interim_added_cart_page.py

--- app/interim_added_cart_page.py
+++ app/interim_added_cart_page.py
@@ -1,6 +1,9 @@
 from flask import session, render_template, request, redirect
 from flask_login import current_user
 from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField
+from wtforms.validators import ValidationError, DataRequired, EqualTo, InputRequired, NumberRange
+from flask_babel import _, lazy_gettext as _l
 
 
 import datetime
