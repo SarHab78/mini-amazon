93f332be47a2276b54cca9d1fca26acf802a370e
Flannery Nania <fpn@duke.edu>
2021-12-05 12:53:26 -0500

    updating reviews; seller reviews

--- app/reviews.py
+++ app/reviews.py
@@ -8,7 +8,7 @@ from werkzeug.urls import url_parse
 from flask_login import login_user, logout_user, current_user
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField
-from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from wtforms.validators import ValidationError, DataRequired, EqualTo, InputRequired, NumberRange
 from flask_babel import _, lazy_gettext as _l
 import datetime 
 import uuid
@@ -24,14 +24,21 @@ bp = Blueprint('reviews', __name__)
 
 class reviews(FlaskForm):
     rid = StringField(_l('rid'), validators=[DataRequired()])
-    email = StringField(_l('email'), validators=[DataRequired()])
-    rating = IntegerField(_l('rating'), validators=[DataRequired()])
     pid = IntegerField(_l('pid'), validators=[DataRequired()])
-    review = StringField(_l('review'), validators=[DataRequired()])
     uid = IntegerField(_l('uid'), validators=[DataRequired()])
-    timestamp = StringField(_l('timestamp'), validators=[DataRequired()])
+    email = StringField(_l('email'), validators=[DataRequired()])
+    rev_timestamp = StringField(_l('rev_timestamp'), validators=[DataRequired()])
+    rating = IntegerField(_l('rating'), validators=[DataRequired(), NumberRange(min=1, max = 5, message="Invalid range")])
+    review = StringField(_l('review'), validators=[DataRequired()])
     submit = SubmitField(_l('Submit'))
     # def validate_email(self, email): when we have cart functionality?
+
+    #validate review 
+    #def validate_review(self, pid):
+     #   if Product_review.review_exists(pid, current_user.id):
+      #      return True
+       # else: 
+        #    return False
 
 @bp.route('/<product_id>/review_form', methods=['GET', 'POST'])
 def add_review(product_id):
@@ -44,7 +51,7 @@ def add_review(product_id):
     
     #autopopulate timestamp
     ct = datetime.datetime.now()
-    form.timestamp.data = ct
+    form.rev_timestamp.data = ct
 
     #autogenerate review id here? -- needs to be unique?, don't know if this is actually a valid way of doing it but o well
     gen_rid = uuid.uuid4()
@@ -55,35 +62,32 @@ def add_review(product_id):
     if request.method == 'GET':
         form.pid.data = page_product[0].product_id
 
-    #validate reviewer
-   # def validate_review(self, pid, uid):
-    #    if Product.product_exists(product_name, current_user.id):
-     #       #raise ValidationError(_('Already a product with this name. Update its quantity instead'))
-      #      return True
-       # else: 
-        #    return False
+
     #actually submit form
     
     if form.validate_on_submit():
+        #print(form.validate_review(form.pid.data))
+        #if form.validate_review(form.pid.data) is False:
+
                 rid = request.form['rid']
                 print(rid)
-                uid = request.form['uid']
-                print(uid)
                 pid = request.form['pid']
                 print(pid)
+                uid = request.form['uid']
+                print(uid)
                 email = request.form['email']
                 print(email)
-                timestamp = request.form['timestamp']
-                print(timestamp)
+                rev_timestamp = request.form['rev_timestamp']
+                print(rev_timestamp)
                 rating = request.form['rating']
                 print(rating)
                 review = request.form['review']
                 print(review)
                 Add_review.add_review(rid,
-                            uid,
                             pid,
+                            uid,
                             email,
-                            timestamp,
+                            rev_timestamp,
                             rating,
                             review)
                 flash('thanks for submitting your review!')
