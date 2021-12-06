d34a3713accc056795ed5bd5018d49ba1982e340
rnt6 <rnt6@duke.edu>
2021-12-06 16:34:37 -0500

    Update base_model.py

--- app/models/base_model.py
+++ app/models/base_model.py
@@ -94,8 +94,8 @@ WHERE id = :id
         #try:
             rows = app.db.execute("""
 UPDATE Users
-SET(email=:email, pwd=:password, firstname=:firstname, lastname=:lastname, address=:address, balance=:balance, is_seller=:is_seller)
-WHERE id=:id
+SET(email= :email, pwd= :password, firstname= :firstname, lastname= :lastname, address= :address, balance= :balance, is_seller= :is_seller)
+WHERE id= :id
 RETURNING id, email, pwd, firstname, lastname, address, balance, is_seller
 """,
                                   id=id,
