ecca9cc601b7472836b90720b0b000d4d2167aa1
js890 <js890@duke.edu>
2021-12-07 11:15:07 -0500

    Update base_model.py

--- app/models/base_model.py
+++ app/models/base_model.py
@@ -205,7 +205,7 @@ WHERE product_id = :product_id
     ''', 
                                 product_id = product_id
                                             )
-        return [Products(*(rows[0])) ]
+        return [Product(*(rows[0])) ]
 
     @staticmethod
     def get_order_image(product_id):
@@ -217,7 +217,7 @@ WHERE product_id = :product_id
                                 product_id = product_id
 
                                             )
-        return [Products(*(rows[0]))]
+        return [Product(*(rows[0]))]
 
     @staticmethod
     def get_seller_products(id):
