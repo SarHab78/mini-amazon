be64bc5273ebfbe51cd92b376a4fe1da2081937f
rnt6 <rnt6@duke.edu>
2021-12-06 22:41:19 -0500

    Update seller_information.py

--- app/seller_information.py
+++ app/seller_information.py
@@ -12,6 +12,6 @@ from .models.base_model import User
 from flask import Blueprint
 bp = Blueprint('seller_information', __name__)
 
-@bp.route('/sellerinformation>', methods=['GET', 'POST'])
+@bp.route('/seller_information>', methods=['GET', 'POST'])
 def seller_information():
     return render_template('seller_information.html')
