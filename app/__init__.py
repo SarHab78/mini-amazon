1dc66216f180f3e1fe0b374bff5b7d6d1a6a3260
rnt6 <rnt6@duke.edu>
2021-12-07 12:36:19 -0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -19,6 +19,7 @@ def create_app():
     app.config['MAIL_USE_TLS'] = True
     app.config['MAIL_USERNAME'] = 'annikakessler2@gmail.com'
     app.config['MAIL_PASSWORD'] = 'emmasutton2'
+    mail = Mail(app)
     
 
     # ADD A SECRET KEY LINE - SOPHIE - this is to try to make "session" work
