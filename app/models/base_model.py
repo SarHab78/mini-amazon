98703d7b32b0e238863bd8b66e0c37e15a3831a1
rnt6 <rnt6@duke.edu>
2021-12-06 21:58:32 -0500

    Update base_model.py

--- app/models/base_model.py
+++ app/models/base_model.py
@@ -851,7 +851,7 @@ RETURNING rid
             return None
 
 class Prod_Sell_Rev_Cat:
-    def __init__(self, product_name, product_id, product_description, image_url, price, quantity, firstname, lastname, available, avg_rating, cat_name):
+    def __init__(self, product_name, product_id, product_description, image_url, price, quantity, firstname, lastname, available, avg_rating, cat_name, email, address):
         self.product_id = product_id
         self.product_name = product_name
         self.product_description = product_description
