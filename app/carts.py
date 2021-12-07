acdb04a4e45fe739326faa9901f90059cc6521c2
js890 <js890@duke.edu>
2021-12-07 18:00:16 -0500

    Update carts.py

--- app/carts.py
+++ app/carts.py
@@ -44,9 +44,8 @@ def checkout():
     if form.validate_on_submit():
         print('made it')
         add_date = request.form['add_date']
-    if form.is_submitted() and not form.validate():
+    else:        
         print('not submitted')
-
         flash('Error, please try again later')
     checking_out = Orders.checkout_cart(current_user.id, add_date)
     return render_template('checkout.html', checking_out)
