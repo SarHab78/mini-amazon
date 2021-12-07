9d2d1f7064f37f27a0d592f2ee73065e16af5fad
rnt6 <rnt6@duke.edu>
2021-12-06 22:12:37 -0500

    Update product_page.py

--- app/product_page.py
+++ app/product_page.py
@@ -44,6 +44,7 @@ def product_page(name, product_id):
     avg_product_rating = Product_review.avg_product_rating(pid = product_id)
     num_reviews = Product_review.count_prod_reviews(pid = product_id)
     quant_options = Prod_Sell_Rev_Cat.get_quant_list(product_id = product_id)
+    seller_information = Product.get_information(product_id=product_id)
 
     time = datetime.datetime.now()
     form.add_date.data = time
@@ -71,4 +72,5 @@ def product_page(name, product_id):
                             name = name,
                             product_id = product_id,
                             quant_options = quant_options,
+                            seller_information=seller_information,
                             form=form)
