049119f5bdb64690efc5b9f8b2bb20565fa056c1
rnt6 <rnt6@duke.edu>
2021-12-07 10:40:46 -0500

    Update users.py

--- app/users.py
+++ app/users.py
@@ -3,7 +3,7 @@ from werkzeug.urls import url_parse
 from flask_login import login_user, logout_user, current_user
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField, DecimalField
-from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, AnyOf
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, AnyOf, NoneOf, message
 from flask_babel import _, lazy_gettext as _l
 
 from .models.base_model import User
