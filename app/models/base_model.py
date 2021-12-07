48174811debf31f4b557b00e56a9f6f53d1ea54a
js890 <js890@duke.edu>
2021-12-06 23:09:59 -0500

    Update base_model.py

--- app/models/base_model.py
+++ app/models/base_model.py
@@ -607,7 +607,7 @@ WHERE Orders.prod_id = Products.product_id AND Orders.ordered = 'N' AND Orders.u
                                 quantity = quantity,
                                 add_date = add_date
             )
-        return Orders.get_cart(uid, prod_id)
+        return Orders.get_cart(uid)
  
     @staticmethod
     def total_price(uid):
