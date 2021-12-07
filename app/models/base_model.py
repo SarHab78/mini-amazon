8b1494a8592d9834f779c24ea2e6154c51787e92
rnt6 <rnt6@duke.edu>
2021-12-07 15:10:58 -0500

    Update base_model.py

--- app/models/base_model.py
+++ app/models/base_model.py
@@ -1216,7 +1216,7 @@ class total_money:
         self.ordered=ordered
     
     @staticmethod
-    def total_spent(uid):
+    def total_money(uid):
         rows = app.db.execute('''
 SELECT total
 FROM total_money
