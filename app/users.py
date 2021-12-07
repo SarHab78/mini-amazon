bb257e57f07d3a5d0bdd7863da8ca120adf664c8
rnt6 <rnt6@duke.edu>
2021-12-07 12:43:17 -0500

    Update users.py

--- app/users.py
+++ app/users.py
@@ -1,10 +1,11 @@
-from flask import render_template, redirect, url_for, flash, request
+from flask import render_template, redirect, url_for, flash, request, mail
 from werkzeug.urls import url_parse
 from flask_login import login_user, logout_user, current_user
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField, DecimalField
 from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, AnyOf, NoneOf
 from flask_babel import _, lazy_gettext as _l
+from flask_mail import Message
 
 from .models.base_model import User
 
@@ -168,7 +169,11 @@ class ResetPasswordForm(FlaskForm):
     submit = SubmitField('Reset Password')
 
 def send_reset_email(user):
-    pass
+    token = User.get_reset_token()
+    msg=Message('Password Reset: Mini Amazon', sender='noreply@steministgirlboss.com', recipients=[user.email()])
+    msg.body=f'''To reset your password, visit the following link: {url_for{'Users.reset_token', token=token, _external=True}}
+
+    If you did not make this request, please disregard this email.'''
 
 @bp.route('/reset_password', methods=['GET', 'POST'])
 def reset_request():
@@ -179,7 +184,11 @@ def reset_request():
         user = User.get_from_email(email=form.email.data)
         send_reset_email(user)
         flash('An email has been sent with instructions to reset your password', 'info')
-        return redirect(url_for('Users.login'))
+        if form.validate_on_submit():
+            if User.edit_password(
+                        current_user.id,
+                        form.password.data):
+        return render_template("login.html", form=form, id=id)
     return render_template('reset_request.html', title='Reset Password', form=form)
 
 @bp.route("/reset_password/<token>")
