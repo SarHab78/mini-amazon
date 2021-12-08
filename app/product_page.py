00995cbf2ae4bfeb54194bbc571e026c86961cc5
Sophia Vincoff <slv19@duke.edu>
2021-12-08 01:53:34 -0500

    fixed up a few more things

--- app/product_page.py
+++ app/product_page.py
@@ -38,11 +38,7 @@ def product_page(name, product_id):
     name = name
     product_id = product_id
     searched_products = Prod_Sell_Rev_Cat.get_search_result(search_str='book')     
-<<<<<<< HEAD
     purchases = Orders.past_orders(current_user.id)
-=======
-    purchases = None
->>>>>>> 171f0b068649f11570cb22af8df23a17a08be2b3
     products_by_other_sellers = Prod_Sell_Rev_Cat.get_products_by_other_sellers(product_id=product_id)
     seller = Product.get_product_for_page(product_id = product_id)
     page_product = Prod_Sell_Rev_Cat.get_sell_rev_info(product_id = product_id)
@@ -76,7 +72,10 @@ def product_page(name, product_id):
         else:
             return interim(current_user.id, name, product_id, quant_selected, add_date)
     if form.is_submitted() and not form.validate():
-        flash('Invalid - must enter a value between 0 and {}'.format(quant_options[-1]))
+        if len(quant_options) > 0:
+            flash('Invalid - must enter a value between 0 and {}'.format(quant_options[-1]))
+        else:
+            flash('Invalid - none left') 
 
     return render_template('product_page.html', 
                             products_by_other_sellers = products_by_other_sellers,
